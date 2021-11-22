5e098be0e82ba7ffd4e5c9e4edc4d5e71b060587
Samuel Jackson <dsiq3g@gmail.com>
2021-11-22 02:21:02 -0800

    Added show option to visualize the memory

--- memallook/src/main/java/com/hiring/memallook/commands/Display.java
+++ memallook/src/main/java/com/hiring/memallook/commands/Display.java
@@ -1,6 +1,15 @@
 package com.hiring.memallook.commands;
 
+import com.google.common.collect.Lists;
+import com.hiring.memallook.data.Memory;
+import com.hiring.memallook.data.MemoryBlock;
+import com.hiring.memallook.io.FileHandler;
 import picocli.CommandLine;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @CommandLine.Command(name = "show",
         description = "Prints the simulated memory to the screen in a human " +
@@ -9,6 +18,31 @@ public class Display implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Running Display");
+        FileHandler fileHandler = new FileHandler();
+        Memory mem;
+        try {
+            mem = fileHandler.readMemoryFromFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        List<String> outArray = new ArrayList<>(Collections.nCopies(mem.numPages, "."));
+
+        mem.memoryBlockList
+                .forEach(blk -> {
+                    for (int i = blk.start; i < blk.end; i++) {
+                        outArray.set(i, blk.tag);
+                    }
+                });
+        List<List<String>> squaredArray = Lists.partition(outArray, 10);
+        for (List<String> strings : squaredArray)
+            System.out.println(String.join("", strings));
+
+        System.out.println("");
+        System.out.println("<Allocations by tags>");
+        for (MemoryBlock blk : mem.memoryBlockList) {
+            System.out.format("%s:\t%d bytes\n", blk.tag, (blk.end - blk.start) * mem.pageSize);
+        }
     }
 }
